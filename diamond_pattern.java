b1277b68bc69bbc39b9f97c10cff15ff6dd30650
darshitasojitra <darshitasojitra21@gmail.com>
2025-07-31 12:50:01 +0530

    Add files via upload
    add

--- diamond_pattern.java
+++ diamond_pattern.java
@@ -1,20 +1,20 @@
 public class diamond_pattern {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int n=5;
         for(int i=1;i<=n;i++){
-            for(int j=i;j<n;j++){
+            for(int j=i;j<=n;j++){
                 System.out.print(" ");
             }
-            for(int j=1;j<=(i*2-1);j++){
+            for(int j=1;j<=(2*i-1);j++){
                 System.out.print("*");
             }
             System.out.println();
         }
-        for(int i=n-1;i>=1;i--){
-            for(int j=n;j>i;j--){
+        for(int i=n;i>=1;i--){
+            for(int j=n;j>=i;j--){
                 System.out.print(" ");
             }
-            for(int j=1;j<=(i*2-1);j++){
+            for(int j=1;j<=(2*i-1);j++){
                 System.out.print("*");
             }
             System.out.println();
